949d698d7113551a3acf1318340009329b58dacb
bsu-wrudisill <rudisill@login01.chn.perlmutter.nersc.gov>
2024-10-01 16:25:22 -0700

    add aso regrid script

---
+++ ASO/regrid_aso_ll.py
@@ -0,0 +1,22 @@
+import xarray as xr
+import numpy as np
+import xesmf as xe
+import pathlib as pl 
+import sys 
+
+
+aso_file = pl.Path(sys.argv[1])
+
+aso = xr.open_dataset(aso_file)
+
+# open up the target file 
+ds = xr.open_dataset("bl_wy2019.out.clm_output.04512.sa_regridll.nc")
+
+# regrid the to the regular lat lon grid in the parflow file 
+regridder = xe.Regridder(aso, ds, 'bilinear', ignore_degenerate=True)
+
+# the output name 
+the_name = aso_file.name.split(".nc")[0] + "regrid_reg.nc"
+
+aso_regrid = regridder(aso)
+aso_regrid.to_netcdf(the_name)
